2a8e91017a144539fe1a281d8c65ef44a21b5e6f
Diogo Izele <diogogabrielizele@gmail.com>
2022-06-19 18:25:29 -0300

    feat(theme): creates light mode theme values

---
+++ src/styles/theme/lightMode.ts
@@ -0,0 +1,88 @@
+export const lightModePalette = {
+  primary: {
+    main: "#50379c",
+  },
+  secondary: {
+    main: "#9467cc",
+  },
+  info: {
+    main: "#b99dfa",
+  },
+  success: {
+    main: "#0ca864",
+  },
+  warning: {
+    main: "#e1cd0c",
+  },
+  error: {
+    main: "#f02404",
+  },
+  background: {
+    default: "#CFCCE8",
+    contrast: "#FFF5FF",
+  },
+  common: {
+    white: "#ffffff",
+    black: "#222222",
+  },
+  text: {
+    primary: "#2b2b2b",
+    secondary: "#9b9b9b",
+    disabled: "#9b9b9b",
+    hint: "#9b9b9b",
+  },
+};
+
+export const lightModeShadows: [
+  "none",
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string
+] = [
+  "none",
+  "0px 1px 3px 0px #66666633, 0px 1px 1px 0px #4b4b4b23, 0px 2px 1px -1px #4b4b4b1e",
+  "0px 1px 5px 0px #66666633, 0px 2px 2px 0px #4b4b4b23, 0px 3px 1px -2px #4b4b4b1e",
+  "0px 1px 8px 0px #66666633, 0px 3px 4px 0px #4b4b4b23, 0px 3px 3px -2px #4b4b4b1e",
+  "0px 2px 4px -1px #66666633, 0px 4px 5px 0px #4b4b4b23, 0px 1px 10px 0px #4b4b4b1e",
+  "0px 3px 5px -1px #66666633, 0px 5px 8px 0px #4b4b4b23, 0px 1px 14px 0px #4b4b4b1e",
+  "0px 3px 5px -1px #66666633, 0px 6px 10px 0px #4b4b4b23, 0px 1px 18px 0px #4b4b4b1e",
+  "0px 4px 5px -2px #66666633, 0px 7px 10px 1px #4b4b4b23, 0px 2px 16px 1px #4b4b4b1e",
+  "0px 5px 5px -3px #66666633, 0px 8px 10px 1px #4b4b4b23, 0px 3px 14px 2px #4b4b4b1e",
+  "0px 5px 6px -3px #66666633, 0px 9px 12px 1px #4b4b4b23, 0px 3px 16px 2px #4b4b4b1e",
+  "0px 6px 6px -3px #66666633, 0px 10px 14px 1px #4b4b4b23, 0px 4px 18px 3px #4b4b4b1e",
+  "0px 6px 7px -4px #66666633, 0px 11px 15px 1px #4b4b4b23, 0px 4px 20px 3px #4b4b4b1e",
+  "0px 7px 8px -4px #66666633, 0px 12px 17px 2px #4b4b4b23, 0px 5px 22px 4px #4b4b4b1e",
+  "0px 7px 8px -4px #66666633, 0px 13px 19px 2px #4b4b4b23, 0px 5px 24px 4px #4b4b4b1e",
+  "0px 7px 9px -4px #66666633, 0px 14px 21px 2px #4b4b4b23, 0px 5px 26px 4px #4b4b4b1e",
+  "0px 8px 9px -5px #66666633, 0px 15px 23px 2px #4b4b4b23, 0px 6px 28px 5px #4b4b4b1e",
+  "0px 8px 10px -5px #66666633, 0px 16px 25px 2px #4b4b4b23, 0px 6px 30px 5px #4b4b4b1e",
+  "0px 8px 11px -5px #66666633, 0px 17px 28px 2px #4b4b4b23, 0px 6px 32px 5px #4b4b4b1e",
+  "0px 9px 11px -5px #66666633, 0px 18px 31px 3px #4b4b4b23, 0px 7px 34px 6px #4b4b4b1e",
+  "0px 9px 12px -6px #66666633, 0px 19px 34px 3px #4b4b4b23, 0px 7px 36px 6px #4b4b4b1e",
+  "0px 10px 13px -6px #66666633, 0px 20px 37px 3px #4b4b4b23, 0px 8px 40px 7px #4b4b4b1e",
+  "0px 10px 13px -6px #66666633, 0px 21px 40px 3px #4b4b4b23, 0px 8px 42px 7px #4b4b4b1e",
+  "0px 10px 14px -6px #66666633, 0px 22px 43px 3px #4b4b4b23, 0px 8px 44px 7px #4b4b4b1e",
+  "0px 11px 14px -7px #66666633, 0px 23px 46px 3px #4b4b4b23, 0px 9px 48px 8px #4b4b4b1e",
+  "0px 11px 15px -7px #66666633, 0px 24px 49px 3px #4b4b4b23, 0px 9px 51px 8px #4b4b4b1e",
+];

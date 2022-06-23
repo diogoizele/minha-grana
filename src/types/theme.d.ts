2e0ced21c215a4037a87d255e1a2518b384ef6fd
Diogo Izele <diogogabrielizele@gmail.com>
2022-06-22 23:49:09 -0300

    feat(theme): increments theme types

--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -1,14 +1,17 @@
 import * as Theme from "@mui/material";
 
 import type { ThemePalette } from "styles/theme/theme.types";
+import type { TypographyOptions } from "@mui/material/styles/createTypography";
 
 declare module "@mui/material" {
   interface Theme {
     palette: ThemePalette;
+    typography: TypographyOptions;
   }
 
   interface ThemeOptions extends Partial<Theme.Theme> {
     palette?: ThemePalette;
+    typography: TypographyOptions;
   }
 }
 

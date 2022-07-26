362d8b9159da5e84cfdeb69b395b9eaa5064e681
Diogo Izele <diogo.izele@cwi.com.br>
2022-07-26 10:02:09 -0300

    refactor: decreases top bar shadow

--- src/shared/topBar/topBar.styles.ts
+++ src/shared/topBar/topBar.styles.ts
@@ -6,6 +6,6 @@ export const Container = styled("header")(({ theme }) => ({
 
   backgroundColor: theme.palette.background.contrast,
 
-  boxShadow: "0 0.2rem 1rem rgba(0, 0, 0, 0.1)",
+  boxShadow: "0 0.2rem 0.1rem rgba(0, 0, 0, 0.1)",
   borderRadius: "0.5rem",
 }));
